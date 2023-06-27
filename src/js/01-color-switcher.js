54f89305f0500ece83b3ff678fa3ecf8ddbcd79b
Galyna Shevchenko <galu4ok@gmail.com>
2023-06-27 03:30:12 +0300

    add task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+// Завдання 1 - перемикач кольорів
+// Напиши скрипт, який після натискання кнопки «Start», раз на секунду змінює колір фону < body > на випадкове значення,
+// використовуючи інлайн стиль.Натисканням на кнопку «Stop» зміна кольору фону повинна зупинятися.
+// Для генерування випадкового кольору використовуй функцію getRandomHexColor.
 
+const COLORS_DELAY = 1000;
+let intervalId = null;
+
+const startBtnRef = document.querySelector('button[data-start]');
+const stopBtnRef = document.querySelector('button[data-stop]');
+const bodyRef = document.body;
+
+startBtnRef.addEventListener('click', onStartBtnClick);
+stopBtnRef.addEventListener('click', onStopBtnClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function onStartBtnClick() {
+  startBtnRef.disabled = true;
+  stopBtnRef.disabled = false;
+  intervalId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, COLORS_DELAY);
+}
+
+function onStopBtnClick() {
+  startBtnRef.disabled = false;
+  stopBtnRef.disabled = true;
+  clearInterval(intervalId);
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,8 @@
+// Завдання 3 - генератор промісів
+// Напиши скрипт, який на момент сабміту форми викликає функцію createPromise(position, delay) стільки разів,
+// скільки ввели в поле amount.Під час кожного виклику передай їй номер промісу(position), що створюється,
+// і затримку, враховуючи першу затримку(delay), введену користувачем, і крок(step).
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
